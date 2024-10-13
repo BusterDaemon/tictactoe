22d89f1065500e2bce5a447a16a5c85efbd5ac14
Buster Daemon <bugsbunnygdi@pm.me>
2024-10-13 16:26:48 +0300

    Добавлено главное меню

--- main.cxx
+++ main.cxx
@@ -1,8 +1,37 @@
 #include "./src/tictactoe.hxx"
+#include <stdio.h>
+#include <stdint.h>
 
 int main(void) {
-  TicTacToe game(3);
-  game.Run();
+  uint32_t size = 0;
+  uint32_t response = 0;
 
+  while (1) {
+    puts("Добро пожаловать в игру!");
+    puts("Какого размера поле ты хочешь себе?");
+    fscanf(stdin, "%u", &size);
+    if (size < 3) {
+      puts("Слишком малый размер");
+      continue;
+    }
+    if (size > 10) {
+      puts("Слишком большой размер");
+      continue;
+    }
+    
+    TicTacToe game(size);
+    game.Run();
+
+    puts("Ещё партейку?");
+    puts("1. Да");
+    puts("2. Нет");
+    fscanf(stdin, "%u", &response);
+    if (response == 1) {
+      continue;
+    }
+    if (response == 2) {
+      break;
+    }
+  }
   return 0;
 }
