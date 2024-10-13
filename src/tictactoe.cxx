3571df46f02bacec1e788be8b6f4fabadedbaaf5
Buster Daemon <bugsbunnygdi@pm.me>
2024-10-13 08:45:19 +0300

    Добавлена возможность совершить ход

--- src/tictactoe.cxx
+++ src/tictactoe.cxx
@@ -20,6 +20,15 @@ TicTacToe::~TicTacToe() {
   free(this->board);
 }
 
+bool TicTacToe::Move(int x, int y) {
+  if ((x > this->boardSize || x < 0) || (y > this->boardSize || y < 0) ||
+      this->board[x][y] != 0)
+    return false;
+  this->board[x][y] = this->curPlayer ? 1 : 0; // 1 - крест, 0 - нуль
+  this->curPlayer = !this->curPlayer;
+  return true;
+}
+
 void TicTacToe::Run() {
   for (size_t i = 0; i < this->boardSize; i++) {
     for (size_t j = 0; j < this->boardSize; j++) {
